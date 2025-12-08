6d33eb4ca6c5e513205219d5aa14f53db8e9d95e
5ee5 <marks.sislovs@gmail.com>
2025-12-08 08:43:59 +0200

    Initial commit

---
+++ num.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int year_of_birth;
+string name;
+bool flag = false;
+int current_year = 2025;
+
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Enter your year of birth: ";
+    cin >> year_of_birth;
+    int age = current_year - year_of_birth;
+    
+    cout << "Hello, " << name << endl << "You are "<< age << " years old!" << endl;
+    return 0;
+}
