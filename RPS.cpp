a36b7894bbdac189fa85c3eda0fe5ff11865bb8c
5ee5 <marks.sislovs@gmail.com>
2025-12-09 14:44:40 +0200

    replacing vectors with simple arrays

--- RPS.cpp
+++ RPS.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
-#include <vector>
 #include <random>
 using namespace std;
+
 int main() {
     char player;
-
     cout << "Enter Your choice (R/P/S): ";
     cin >> player;
-    vector<char> rps = {'R', 'P', 'S'};
+
+    char choices[] = {'R', 'P', 'S'};
+    int size = 3;
 
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<int> dist(0, rps.size() - 1);
-    char computer = rps[dist(gen)];
+    uniform_int_distribution<int> dist(0, size - 1);
+
+    char computer = choices[dist(gen)];
     cout << "Computer picked: " << computer << endl;
 
     if (computer == player) {
         cout << "It's a tie!" << endl;
     }
-    if ((player == 'R' && computer == 'S') ||
-        (player == 'P' && computer == 'R') ||
-        (player == 'S' && computer == 'P')) {
+    else if ((player == 'R' && computer == 'S') ||
+             (player == 'P' && computer == 'R') ||
+             (player == 'S' && computer == 'P')) {
         cout << "You win!" << endl;
     }
     else {
         cout << "Computer wins!" << endl;
     }
-
-
     return 0;
 }
